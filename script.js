9fa99828d6cd2f060aa80170ac3d0188e942975a
IvanFrade <fradellin.ivan@gmail.com>
2025-06-15 20:27:22 +0200

    Add number buttons functionality

--- script.js
+++ script.js
@@ -28,3 +28,35 @@ function operate(a, b, op) {
             break;
     }
 }
+
+function updateDisplay() {
+    display.textContent = currentExpression;
+}
+
+function clearScreen() {
+    value = "0.0";
+    updateDisplay();
+}
+
+function registerNumber(n) {
+    if (currentExpression === "0.0")
+        currentExpression = "";
+
+    currentExpression += n;
+    
+    updateDisplay();
+}
+
+function initNumberButtons() {
+    const numberButtons = document.querySelectorAll(".number-button");
+    for (let button of numberButtons)
+        button.addEventListener("click", () => {
+            registerNumber(button.textContent);
+        });
+}
+
+let currentExpression = "0.0";
+const display = document.querySelector(".display");
+
+updateDisplay();
+initNumberButtons();
